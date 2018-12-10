fe7ad5d996e68dc1c15344d29bb8df086d8a3dff
excellink <excellink@163.com>
2018-12-10 23:42:28 +0800

    Header [type]: feat: 保存记录功能 [scope]:\ [subject]: 添加输出更名记录功能

--- renamefiles.py
+++ renamefiles.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # rename_files
 # 功能：统一修改文件名，保持原文件类型不变，若非文件则不进行修改
-# version 0.1 获取文件名称 version 0.2.1 更改文件名称
-# version 0.2.2 更改文件名称并保留文件原来格式后缀
 import os
 
 
@@ -19,13 +17,16 @@ def rename_files(file_path):
             file_dir = os.path.join(file_path, file)    # 连接file路径和名字
             f_name, fe_name = os.path.splitext(file_dir)    # 拆分路径文件名与后缀
             new_name = os.path.join(file_path, str_name + str(n)) + fe_name
-            # os.rename(file_dir, new_name)
+            os.rename(file_dir, new_name)
             n = n+1
-            print(file_dir, "=====>", new_name)
+            print(file_dir, "---->", new_name)
+            log.write(file_dir + '---->' + new_name + '\n')
 
 
 filepath = 'D:\\test'
-
+# print(os.getcwd())
+os.chdir('C:\\Users\\excel\\Desktop')
+log = open('ChangeLog.txt','w')
 rename_files(filepath)
-
+log.close()
 
